4643451d534e442a6608094f48ac3faf0b3bca61
mike-fmh <65558188+mike-fmh@users.noreply.github.com>
2021-12-06 10:12:25 -0500

    Update BMS-GS.py

--- runtime graphing/BMS-GS.py
+++ runtime graphing/BMS-GS.py
@@ -1,7 +1,11 @@
+import tqdm
+from matplotlib import pyplot
+import time
+
 def goodProcess(query):
     goodList = []
     border = []
-    f = 0;
+    f = 0
     while f <= len(query):
         goodList.append(0)
         border.append(0)
@@ -53,12 +57,44 @@ def search(text, query):
     return indices
 
 
-def main():
-    text = input()
-    query = input()
-    if len(text) < len(query):
-        print("Query Not Found!")
-    else:
-        print("Query found at the following indices: ")
-        print(search(text, query))
-main()
+
+def gettime(txt, query, xrange, reps, intvl):
+    times = []
+    [times.append(0) for i in range(xrange - len(txt))]  # prefill the list with 0s
+    sizes = []
+    [sizes.append(i) for i in range(xrange - len(txt))]  # prefill with len(txt)-->1000
+
+    for s in tqdm.tqdm(range(reps)):
+        looptxt = txt
+        for i in (range(xrange - len(txt))):
+            if i % intvl == 0:
+                starttime = time.time()
+                m = search(looptxt, query)
+                endtime = time.time()
+                times[i] += endtime - starttime
+                looptxt += "a"
+
+    for i in range(xrange-len(txt)):
+        times[i] /= reps  # compute the average runtimes
+    return times, sizes
+
+
+def createPlot(x, y, xlabel="", ylabel="", graph_label=""):
+    figure, axes = pyplot.subplots()
+    axes.plot(x, y)
+    axes.set_ylabel(ylabel)
+    axes.set_xlabel(xlabel)
+    axes.set_title(graph_label)
+    pyplot.show()
+
+
+if __name__ == '__main__':
+    text = "a"
+    query = "a"
+
+    reps = 10
+    itvl = 10
+    xrange = 500000
+
+    runtimes, textsizes = gettime(text, query, xrange, reps, itvl)
+    createPlot(textsizes, runtimes, "Text Size", "Runtime (secs)", "Graph of runtimes for Boyer-Moore (Good Suffix) String Search")
